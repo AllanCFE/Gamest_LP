bb0c71904e9461d4b47ec5939fc0c54566b1f531
AllanCFE <allan.echeverria@gmail.com>
2022-09-29 23:32:26 -0300

    feat: cover basis ready

--- Components/Cover/Cover.js
+++ Components/Cover/Cover.js
@@ -2,6 +2,9 @@ import styles from '../../styles/Cover.module.css'
 import Image from 'next/image';
 
 export default () => {
+
+    let bulletSize = 50
+
     return (
         <div className={styles.wrap}>
             <img className={styles.bg} src='/Back_Cover.png' alt=""/>
@@ -11,59 +14,59 @@ export default () => {
                     {/** Primeira coluna, 100vh */}
                     <div style={{width:'70%'}}>
                         {/** 1a linha, para o título, contendo 2 colunas */}
-                        <div className={[styles.horizontalSplit, styles.line].join(" ")}>
+                        <div className={[styles.horizontalSplit, styles.line, styles.verticalAlign].join(" ")}>
                             {/** Controle esquerdo */}
-                            <div className={[styles.controles].join(" ")}> 
+                            <div style={{width: "13%"}}> 
                                 <Image src='/Controle_L.png' layout="responsive" width={480} height={1024} />
                             </div>
                             {/** Título */}
-                            <div>
+                            <div style={{paddingLeft: "2vw"}}>
                                 <h1>Conectamos talentos a indústria de games</h1>
                             </div>
                         </div>
                         {/** 2a linha, para o subtítulo, contendo 2 colunas */}
-                        <div className={[styles.horizontalSplit, styles.line].join(" ")}>
+                        <div className={[styles.horizontalSplit, styles.line, styles.verticalAlign].join(" ")}>
                             {/** Subtítulo */}
-                            <div style={{paddingLeft: '15vw'}}>
+                            <div style={{paddingRight:"2vw", textAlign: "right", marginLeft: "10vw"}}>
                                 <h2>Buscamos de forma personalizada o profissional perfeito para alavancar o projeto do seu game</h2>
                             </div>
                             {/** Controle direito */}
-                            <div>
-                                <Image src='/Controle_R.png' width={79} height={160}/>
+                            <div style={{width: "15%"}}>
+                                <Image src='/Controle_R.png'  layout="responsive" width={480} height={1024}/>
                             </div>
                         </div>
                         {/** 3a linha, para os bullets, contendo 3 colunas */}
-                        <div className={[styles.horizontalSplit, styles.line].join(" ")} style={{justifyContent:"space-between"}}>
+                        <div className={[styles.horizontalSplit, styles.line, styles.bullet].join(" ")} style={{justifyContent:"space-between"}}>
                             {/** Bullet 1 */}
                             <div className={[styles.horizontalSplit, styles.verticalAlign].join(" ")}>
                                 {/** Imagem */}
                                 <div>
-                                    <Image src='/Clock.png' width={40} height={40}/>  
+                                    <Image src='/Clock.png' width={bulletSize} height={bulletSize}/>  
                                 </div>
                                 {/** Texto */}
-                                <div>
+                                <div className={[styles.bulletText].join(" ")}>
                                     Rápido
                                 </div>
                             </div>
                             {/** Bullet 2 */}
-                            <div className={[styles.horizontalSplit, styles.verticalAlign].join(" ")}>
+                            <div className={[styles.horizontalSplit, styles.verticalAlign, styles.bullet].join(" ")}>
                                 {/** Imagem */}
                                 <div>
-                                    <Image src='/Money.png' width={40} height={40}/>            
+                                    <Image src='/Money.png' width={bulletSize} height={bulletSize}/>            
                                 </div>
                                 {/** Texto */}
-                                <div>
+                                <div className={[styles.bulletText].join(" ")}>
                                     Barato
                                 </div>
                             </div>
                             {/** Bullet 3 */}
-                            <div className={[styles.horizontalSplit, styles.verticalAlign].join(" ")}>
+                            <div className={[styles.horizontalSplit, styles.verticalAlign, styles.bullet].join(" ")}>
                                 {/** Imagem */}
                                 <div>
-                                    <Image src='/Control.png' width={40} height={40}/>  
+                                    <Image src='/Control.png' width={bulletSize} height={bulletSize}/>  
                                 </div>
                                 {/** Texto */}
-                                <div>
+                                <div className={[styles.bulletText].join(" ")}>
                                     Pensado pra você...
                                 </div>
                             </div>
@@ -97,7 +100,7 @@ export default () => {
                     </div>
 
                     {/** Segunda coluna com apenas o tamanho da figura */}
-                    <div className={[styles.secondColumn].join(" ")} style={{width:'30%'}}>
+                    <div className={[styles.secondColumn].join(" ")} style={{width:'30%', paddingTop: "2vh", marginLeft: "1vw"}}>
                         <Image src='/Right_Cover.png' width={250} height={250}/>
                     </div>
                 </div>
