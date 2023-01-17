66481edad76ab58176b0c97330f3351f16f33647
AllanCFE <allan.echeverria@gmail.com>
2023-01-17 10:27:36 -0300

    Correction whatsapp

--- Components/ContactForm/ContactForm.js
+++ Components/ContactForm/ContactForm.js
@@ -68,7 +68,7 @@ export default () => {
                             </a>
                         </div>
                         <div className={styles.smLogos}>
-                            <a href='https://api.whatsapp.com/send?phone=5565999780487&text=Gostaria%20de%20conversar%20sobre%20o%20servi%C3%A7o%20de%20recrutamento%20da%20Gamest' target="_blank" className={styles.centerH}>
+                            <a href='https://api.whatsapp.com/send?phone=556599780487&text=Gostaria%20de%20conversar%20sobre%20o%20servi%C3%A7o%20de%20recrutamento%20da%20Gamest' target="_blank" className={styles.centerH}>
                                 <Image src='/WhatsApp.png' width={100} height={100}/>
                             </a>
                         </div>
