228354613bacac53ddaea751fb233f7375128355
AllanCFE <allan.echeverria@gmail.com>
2023-01-18 13:34:40 -0300

    Assets insertion and creation of ImageLink component

---
+++ Components/ImageLink/ImageLink.tsx
@@ -0,0 +1,43 @@
+// Create a Nextjs component that renders an image with a link
+// Path: Components/ImageLink/ImageLink.tsx
+// Compare this snippet from Components/Footer/Footer.tsx:
+import styles from '../../styles/Footer.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Props {
+    src: string;
+    width: number;
+    height?: number;
+}
+
+interface Local extends Props {
+    href?: never;
+    local: `/${string}`;
+}
+
+interface Out extends Props {
+    local?: never;
+    href: string;
+}
+export default ({src, href, local, width, height=width}:Local | Out) => {
+
+    if(href != undefined){
+        return(
+            <div className={[styles.outLink].join(" ")}>
+                <a href={href} target="_blank">
+                    <Image src={src} width={width} height={height}/>
+                </a>
+            </div>
+        )
+    } else {
+        return(
+        <div className={[styles.localLink].join(" ")}>
+            <Link href={local}>
+                <a><Image src={src} width={width} height={height}/></a>
+            </Link>
+        </div>
+    )
+
+    }
+}
