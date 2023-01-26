0062771061eac004cb18c936f139ac34f8e67b5e
AllanCFE <allan.echeverria@gmail.com>
2023-01-25 23:04:56 -0300

    Add environment variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,9 @@ import { AppProps } from 'next/app';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZumn-Ii27Svxr7IO_sq6LpC0UjXfXojA",
-  authDomain: "gamest-br.firebaseapp.com",
-  projectId: "gamest-br",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: "gamest-br.appspot.com",
   messagingSenderId: "121601058091",
   appId: "1:121601058091:web:47442b26c162b7f7c2916e",
