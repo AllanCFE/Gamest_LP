b5f94922de9da3b2c6c36b42ac1693eb2acd854f
AllanCFE <allan.echeverria@gmail.com>
2023-01-26 16:32:32 -0300

    Deattach of Firebase implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,8 @@
 import '../styles/globals.css'
-import { initializeApp, getApps } from 'firebase/app';
 import { Analytics } from '@vercel/analytics/react';
 import GAnalytics from '../Components/GAnalytics/GAnalytics';
 import { AppProps } from 'next/app';
-
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: "gamest-br.appspot.com",
-  messagingSenderId: "121601058091",
-  appId: "1:121601058091:web:47442b26c162b7f7c2916e",
-  measurementId: "G-L4DVNT9XFT"
-};
-
-
-// Initialize Firebase
-var app;
-if(!getApps().length){
-  app = initializeApp(firebaseConfig);
-}
+import {app} from '../Firebase/Firebase.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
