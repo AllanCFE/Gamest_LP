85ad9ea55a6ca1aba9d71016a19725c22e077af1
AllanCFE <allan.echeverria@gmail.com>
2023-04-06 14:50:14 -0300

    seila

--- Components/CardsArea/CardsArea.tsx
+++ Components/CardsArea/CardsArea.tsx
@@ -2,32 +2,14 @@ import styles from '../../styles/CardsArea.module.css'
 import { useRouter } from 'next/router';
 import localeAssets from '../../pages/assets/texts.json'
 import Image from 'next/image'
-import Link from 'next/link';
 
 export default function CardsArea () {
-
     const { locale } = useRouter();
-
     const txts = localeAssets.front_page.filter(t => t.locale == locale)
-
+    
     return (
-        <div className={[styles.sectionArea].join(" ")}>
-            <div className={`${styles.stripe} ${styles.leftStripe}`}></div>
-            <div className={[styles.holder].join(" ")}>
-                <span className={styles.leftContent}>
-                    <Image src="/PlayerBT.svg" alt="Logo" width={60} height={60} />
-                </span>
-                <span className={styles.rightContent}>
-                    {txts[0].cards.text}
-                </span>
-                <Link href={'/contact'}>
-                    <div className={styles.actionButton}>
-                        <span className={styles.actionText}>{txts[0].cards.button}</span>
-                        <span className={styles.playIcon}></span>
-                    </div>
-                </Link>
-            </div>
-            <div className={`${styles.stripe} ${styles.rightStripe}`}></div>
+        <div className={styles.sectionArea}>
+            Oi
         </div>
     )
 }
